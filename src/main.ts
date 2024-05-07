a7a32538cd2ebfee0d0cbfdc1dec672ac5b2a21e
JB Ferguson <jbferguson85@gmail.com>
2024-05-06 22:07:43 -0500

    Add ending scene.

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,11 @@ async function setupGame() {
         sliceX: 6,
         sliceY: 4,
         anims: {
-            fire: {from: 0, to: 2, speed: 5, loop: true},
-            earth: {from: 18, to: 23, speed: 5, loop: false },
-            notUsed: {from: 12, to: 14, speed: 5, loop: false},
-            wind: { from: 15, to: 16, speed: 15, loop: true},
-            water: {from: 6, to: 11, speed: 5, loop: false}
+            fire: { from: 0, to: 2, speed: 5, loop: true },
+            earth: { from: 18, to: 23, speed: 5, loop: false },
+            notUsed: { from: 12, to: 14, speed: 5, loop: false },
+            wind: { from: 15, to: 16, speed: 15, loop: true },
+            water: { from: 6, to: 11, speed: 5, loop: false }
         }
     });
 
@@ -36,19 +36,19 @@ async function setupGame() {
 
         k.add(startButton);
     });
-    
+
     k.scene("draw", () => {
         const drawButton = makeDrawButton(k)
         drawButton.onClick(() => {
             k.go("card");
         })
-        k.add(drawButton) 
+        k.add(drawButton)
         const scoreLabel = k.add([
             k.text(`Enemy HP: ${globalState.enemyHp}`),
             k.pos(24)
         ]);
         k.onUpdate(() => {
-            scoreLabel.text = `Enemy HP: ${globalState.enemyHp}` 
+            scoreLabel.text = `Enemy HP: ${globalState.enemyHp}`
         });
     });
 
@@ -60,12 +60,38 @@ async function setupGame() {
         console.log(card.element);
         globalState.decrementHp(card.power);
         const elementalEffect = makeElementalAnimation(k, card.element);
-        await k.wait(2, () => {card.destroy()});
+        await k.wait(2, () => { card.destroy() });
         k.add(elementalEffect);
-        await k.wait(2, () => {k.go("draw")})
+        await k.wait(2, () => {
+            if (globalState.enemyHp <= 0) {
+                k.go("end")
+            } else {
+                k.go("draw")
+            }
+        })
     });
 
-    k.go("start");
+    k.scene("end", () => {
+
+        const endText = k.make([
+            k.text("Congratulations! You've defeated the enemy!", {
+                size: 24,
+            }),
+            k.pos(k.width() * 0.5, k.height() * 0.5),
+            k.anchor("center")
+        ]);
+        k.add(endText);
+        const startButton = makeStartButton(k);
+
+        startButton.onClick(() => {
+            globalState.resetState();
+            k.go("draw");
+        });
+        k.add(startButton);
+
+    });
+
+    k.go("end");
 }
 
 setupGame();
--- src/state.ts
+++ src/state.ts
@@ -1,9 +1,13 @@
 export const globalState: {
     enemyHp: number,
-    decrementHp: (amount: number) => void
+    decrementHp: (amount: number) => void,
+    resetState: () => void;
 } = {
     enemyHp: 20,
     decrementHp(amount: number) {
         this.enemyHp -= amount;
+    },
+    resetState() {
+        this.enemyHp = 20;
     }
 }
